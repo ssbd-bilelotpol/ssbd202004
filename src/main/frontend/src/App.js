29aa3810793743da375e1afeff9d18f0c21b6740
Konrad Stępniak <216892@edu.p.lodz.pl>
2020-03-24 17:29:36 +0100

    Dodanie wyswietlania loginu i przypisanych rol na stronie glownej

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -10,49 +10,59 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 import LoginModal from "./components/LoginModal";
 import {useDispatch, useSelector} from "react-redux";
 import {closeAuthModalAction, logoutAction, openAuthModalAction} from "./actions/auth";
+import Container from "@material-ui/core/Container";
 
 
 const useStyles = makeStyles(theme => ({
-  title: {
-    flexGrow: 1
-  },
+    title: {
+        flexGrow: 1
+    },
 }));
 
 function App() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const loggedIn = useSelector(state => state.auth.loggedIn);
-  const open = useSelector(state => state.auth.openModal);
-  const dispatch = useDispatch();
-  const handleOpen = () => {
-    dispatch(openAuthModalAction());
-  };
+    const loggedIn = useSelector(state => state.auth.loggedIn);
+    const open = useSelector(state => state.auth.openModal);
+    const principal = useSelector(state => state.auth.principal);
+    const authorities = useSelector(state => state.auth.authorities);
+    const dispatch = useDispatch();
+    const handleOpen = () => {
+        dispatch(openAuthModalAction());
+    };
 
-  const handleClose = () => {
-    dispatch(closeAuthModalAction());
-  };
+    const handleClose = () => {
+        dispatch(closeAuthModalAction());
+    };
 
-  const logout = () => {
-    dispatch(logoutAction());
-  };
+    const logout = () => {
+        dispatch(logoutAction());
+    };
 
-  return (
-    <div className="App">
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Bilelotpol
-          </Typography>
-          {!loggedIn && <Button color="inherit" onClick={handleOpen}>Login</Button>}
-          {loggedIn && <Button color="inherit" onClick={logout}>Logout</Button>}
-        </Toolbar>
-      </AppBar>
-      <LoginModal open={open} handleClose={handleClose}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Bilelotpol
+                    </Typography>
+                    {!loggedIn && <Button color="inherit" onClick={handleOpen}>Login</Button>}
+                    {loggedIn && <Button color="inherit" onClick={logout}>Logout</Button>}
+                </Toolbar>
+            </AppBar>
+            {loggedIn && <Container>
+                <h2>Welcome {principal}!</h2>
+                <ul>
+                    {authorities.map(auth => <li key={auth}>{auth}</li>)}
+                </ul>
+            </Container>
+            }
+            <LoginModal open={open} handleClose={handleClose}/>
+        </div>
+    );
 }
 
 export default App;
