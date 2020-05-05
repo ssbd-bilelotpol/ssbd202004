1f1f7eb8d6ad1387cca88f128254d96f6fcfba59
Konrad Stępniak <216892@edu.p.lodz.pl>
2020-05-05 18:17:40 +0200

    Merge pull request #48 in SSBD2020/ssbd202004 from feature/SSBD202004-125-potwierdzanie-nieodwracalnych-akcji to master
    Squashed commit of the following:
    
    commit f5d5036f66a4f212564b331ac30cedad74110e41
    Merge: 4db695d b159059
    Author: Adam Zambrzycki <216933@edu.p.lodz.pl>
    Date:   Tue May 5 02:06:51 2020 +0200
    
        Merge branch 'master' of https://atlas.it.p.lodz.pl/bitbucket/scm/ssbd2020/ssbd202004 into feature/SSBD202004-125-potwierdzanie-nieodwracalnych-akcji
    
    commit 4db695d9e8eb7def53a9306d6c2b5ec60dcaef92
    Author: Adam Zambrzycki <216933@edu.p.lodz.pl>
    Date:   Tue May 5 00:06:34 2020 +0200
    
        Fix border radius
    
    commit 46cbd3ad6a8651ed009d3a7e8c8f9f1914b12786
    Author: Adam Zambrzycki <216933@edu.p.lodz.pl>
    Date:   Mon May 4 23:33:10 2020 +0200
    
        Change animation type
    
    commit 3609d9f11786ea345e1ca9e27227c2d50647336d
    Author: Adam Zambrzycki <216933@edu.p.lodz.pl>
    Date:   Mon May 4 23:20:14 2020 +0200
    
        Add animations to confirm button
    
    commit cc63e3d1f474815da903f6715dac74c134832ac6
    Author: Adam Zambrzycki <216933@edu.p.lodz.pl>
    Date:   Mon May 4 22:14:15 2020 +0200
    
        Remove confirmation from account edit
    
    commit 7bf3c93a45ce12d510ab6b6136ccdf2fe2ca427c
    Author: Adam Zambrzycki <216933@edu.p.lodz.pl>
    Date:   Fri May 1 17:45:39 2020 +0200
    
        Add confirmation button

---
+++ src/main/frontend/src/components/controls/ConfirmSubmit.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Label, Button, Transition } from 'semantic-ui-react';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+
+const ExtendingButton = styled(Button)`
+    &&&[data-extended='false'] {
+        border-top-right-radius: 0.28571429rem;
+        border-bottom-right-radius: 0.28571429rem;
+    }
+`;
+
+const ConfirmSubmit = (props) => {
+    const { onSubmit, confirmMessage, cancelText, confirmText, children } = props;
+    const { t } = useTranslation();
+
+    const [visible, setVisible] = useState(false);
+
+    const show = () => setVisible(true);
+    const hide = () => setVisible(false);
+    const onConfirm = () => {
+        onSubmit();
+        hide();
+    };
+
+    return (
+        <div>
+            <Button.Group>
+                <ExtendingButton
+                    type="button"
+                    onClick={visible ? hide : show}
+                    data-extended={visible}
+                    {...props}
+                >
+                    {visible ? cancelText || t('Cancel') : children || t('Save')}
+                </ExtendingButton>
+
+                <Transition visible={visible} animation="fade" duration={500}>
+                    <Button.Or text="or" />
+                </Transition>
+                <Transition visible={visible} animation="fade right" duration={500}>
+                    <Button positive onClick={onConfirm} type="submit" {...props}>
+                        {confirmText || t('Confirm')}
+                    </Button>
+                </Transition>
+            </Button.Group>
+
+            <Transition visible={visible} animation="fade right" duration={500}>
+                <Label color="red" pointing="left">
+                    {confirmMessage || t('This action is irreversible')}
+                </Label>
+            </Transition>
+        </div>
+    );
+};
+
+export default ConfirmSubmit;
