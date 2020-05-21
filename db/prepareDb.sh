754706d84cb474898506ebcf1600723754bed5ed
Konrad Stępniak <216892@edu.p.lodz.pl>
2020-05-21 14:12:28 +0200

    Merge pull request #67 in SSBD2020/ssbd202004 from hotfix/SSBD202004-164-poprawa-ci to master
    Squashed commit of the following:
    
    commit 242a7070d6dc53eb3c5966e446469963d6c69028
    Author: Stefan Żaryn <216940@edu.p.lodz.pl>
    Date:   Thu May 21 14:00:27 2020 +0200
    
        run the whole script in one transaction
    
    commit e26f939a7e5756dcb92f52a09259c79b5927f7b2
    Author: Stefan Żaryn <216940@edu.p.lodz.pl>
    Date:   Thu May 21 13:49:00 2020 +0200
    
        export admin password for the entire script
    
    commit 0794394aa95df4b97653bf37a77613ef3b14d15b
    Author: Stefan Żaryn <216940@edu.p.lodz.pl>
    Date:   Thu May 21 13:43:11 2020 +0200
    
        script exits on sql error

--- db/prepareDb.sh
+++ db/prepareDb.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
+DB_CREATE=$(cat ./dbCreate.sql)
+DB_INIT=$(cat ./dbInit.sql)
 
-PGPASSWORD=$bamboo_DB_PASSWORD psql -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -c "drop owned by $DB_USER;"
-
-PGPASSWORD=$bamboo_DB_PASSWORD psql -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -f ./dbCreate.sql
-
-PGPASSWORD=$bamboo_DB_PASSWORD psql -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -f ./dbInit.sql
+PGPASSWORD=$bamboo_DB_PASSWORD psql -v ON_ERROR_STOP=1 -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -c "BEGIN; drop owned by $DB_USER; $DB_CREATE; $DB_INIT; COMMIT;"
