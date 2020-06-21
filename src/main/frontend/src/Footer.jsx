072144691619fc8d1fd59804395780f61e142ed1
Adam Zambrzycki <216933@edu.p.lodz.pl>
2020-06-21 23:51:18 +0200

    Fix footer color

--- src/main/frontend/src/Footer.jsx
+++ src/main/frontend/src/Footer.jsx
@@ -7,12 +7,13 @@ import { roleColors } from './constants';
 export const BottomSegment = styled(Segment)`
     height: 42px;
     padding: 1em 0em;
+    background: ${(props) => props.color} !important;
 `;
 
 const Footer = () => {
     const role = useSelector((state) => state.auth.user.role);
     return (
-        <BottomSegment inverted vertical style={{ backgroundColor: roleColors[role] }}>
+        <BottomSegment inverted vertical color={roleColors[role] || '#025c83'}>
             <Container>
                 <Header as="h4" inverted>
                     Bilelotpol &copy; 2020
