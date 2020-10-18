6075570bd45c265b6bab8a5027ffa6b9528e273e
Jacek <216890@edu.p.lodz.pl>
2020-10-18 20:39:09 +0200

    add dockerfile for mariadb

--- db/prepareDb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DB_CREATE=$(cat ./dbCreate.sql)
-DB_INIT=$(cat ./dbInit.sql)
-
-PGPASSWORD=$bamboo_DB_PASSWORD psql -v ON_ERROR_STOP=1 -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -c "BEGIN; drop owned by $DB_USER; $DB_CREATE; $DB_INIT; COMMIT;"
