97346c38e883e364f0823bd6d68788ea98308ea4
Jacek <216890@edu.p.lodz.pl>
2020-11-08 21:18:54 +0100

    add nginx proxy

--- python/main.py
+++ python/main.py
@@ -5,14 +5,17 @@ file = "../src/main/webapp/WEB-INF/glassfish-web.xml"
 doc = xml.dom.minidom.parse(file)
 elements = doc.getElementsByTagName("security-role-mapping")
 
+a_dict = {}
+
 output = ""
 
 for role in elements:
     name = role.getElementsByTagName("role-name")[0].firstChild.nodeValue
     groups = [it.firstChild.nodeValue for it in role.getElementsByTagName("group-name")]
-    output += "<security-role name=\"%s\">" % name
     for group in groups:
-        output += "<group name=\"%s\"/>" % group
-    output += "</security-role>\n"
+        if group not in a_dict:
+            a_dict[group] = ""
+        a_dict[group] += "%s," % name
 
-print(output)
+for (key, val) in a_dict.items():
+    print("%s = %s\n" % (key, val))
