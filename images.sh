76a2d75be5466c1cc664fe2aaebf38a8fa3052eb
Konrad Stepniak <vesphy@gmail.com>
2020-12-06 22:58:29 +0100

    Fix image

--- images.sh
+++ images.sh
@@ -10,7 +10,7 @@ cd docker-mariadb && docker build . -t th7nder/bilelotpol-db:0.2  \
     --build-arg MYSQL_USER_AUTH=$MYSQL_USER_AUTH \
     --build-arg MYSQL_PASSWORD_AUTH=$MYSQL_PASSWORD_AUTH  
 cd .. 
-cd docker-wildlfy && docker build . -t th7nder/bilelotpol-wildfly:0.2  \
+cd docker-wildlfy && docker build . -t th7nder/bilelotpol-wildfly:0.3  \
     --build-arg MYSQL_USER_MOK=$MYSQL_USER_MOK \
     --build-arg MYSQL_PASSWORD_MOK=$MYSQL_PASSWORD_MOK  \
     --build-arg MYSQL_USER_MOL=$MYSQL_USER_MOL \
